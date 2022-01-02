07d0960a92c195f176b0e1ea91e3b73c9880cf8a
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-01-02 19:37:39 +0530

    fixed some things in list page

--- src/Stats-Anime/search.js
+++ src/Stats-Anime/search.js
@@ -2,6 +2,7 @@ import react ,{useEffect,useState} from "react";
 import { Link  } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import "./seacrh-style.css";
+import { useQuery } from "react-query";
 
 
 
