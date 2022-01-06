4abb65095ad519f23b521df8d4180f16847945fc
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-01-06 21:51:32 +0530

    completely rewrite of the image slider

--- src/Stats-Anime/mul_img_slider.js
+++ src/Stats-Anime/mul_img_slider.js
@@ -2,7 +2,7 @@ import react from "react";
 import "./animestyle.css";
 import "./topanimestyle.css";
 import { useEffect, useState ,useContext,useRef} from "react";
-import { Link,Outlet,Route,Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 
@@ -11,118 +11,92 @@ export const Mulimgslider =  (prop)=>
     const {items,count} = prop;
     const handle_container = useRef();
     const parent_handle_container = useRef();
-    const [currnum, setCurrnum] = useState(0);
-    const [offsetchild, setOffsetchild] = useState(0);
-    const [offset, setoffset] = useState(0);
-    const [btnset , Setbtn] = useState(false);
-//    console.log(items);
-    const img_slider_container = document.querySelector(".content-inside");
+    const img = useRef();
+    const moreinfo = useRef();
+    const [offsetx , setoffsetx] = useState(0);
+    const [pressed ,ispressed] = useState(false);
+
     useEffect(() => {
         const size = () => {
 
-         
-            handle_container.current.style.transition = "none";
-
-            // --- checks how many cards have their offset left less thatn the container width meaning they are visable in the container
-            const handle_parent = document.querySelector(".item-container");
-            const img_slider_container = document.querySelector(".content-inside");
-
-            let num = [...handle_container.current.children].filter((item) => item.offsetLeft <= img_slider_container.clientWidth).length;
-            setOffsetchild(num);
-
-            //-- if the last card is visable in the parent container then clicking the next button will make the parent container move to it's original position
-            if(count === num)
-            {
-                setoffset(0);
-                return;
-            }
-
-             //-- this conditions are used to recalculate how much the parent should move on resizing the window
-            if (currnum !== 0 && currnum < offsetchild + 2) {
-                setoffset(-170 * currnum);
-               
-            }
-            else if (currnum === 0) {
-                setoffset(-170 * (currnum));
-            }
-            else {
-                setoffset(-170 * (num + 2));
-                setCurrnum(num + 2);
-            }
-            return;
         }
         window.addEventListener("resize", size);
 
-        //--- if th emain parent container is here then recalculate how many childs are visable in the container
-        if(img_slider_container)
-        {
-            setOffsetchild([...handle_container.current.children].filter((item) => item.offsetLeft <= img_slider_container.clientWidth).length);
+
+       
+        window.addEventListener("mouseup", () => {
            
-        }
-
-        //-- a state which becomes true if we are at the initial position and clicked the previous button so the parent container moves very left to show the last card
-       if(btnset)
-       {
-        const recal_visable_childern = [...handle_container.current.children].filter((item) => item.offsetLeft <= img_slider_container.clientWidth).length
-        setCurrnum(count - recal_visable_childern);
-        setoffset(-170 * (count- recal_visable_childern));
-     
-       }
-       
-       
+            ispressed(false);
+        })
 
 
-        return () => {window.removeEventListener("resize", size);Setbtn(false);}
+        return () => {window.removeEventListener("resize", size);}
     });
 
   
 
 
+    function mousemovefn(e)
+    {
+        if (!pressed) {
+            return;
 
-    const gonext = () => {
-        handle_container.current.style.transition = "0.4s ease-in";
+        }
+            e.preventDefault();
+            handle_container.current.style.left = `${e.nativeEvent.offsetX - offsetx}px`;
+            boundery();
+
+    }
     
-
-        let num = currnum;
-        if (num+offsetchild === count) {
-            num = 0;
-            setCurrnum(num);
-            handle_container.current.style.left = "0";
-            return;
+    function boundery()
+    {
+        let outerparent = parent_handle_container.current.getBoundingClientRect();
+        let innerparent = handle_container.current.getBoundingClientRect();
+       
+        if(parseInt(handle_container.current.style.left)>0)
+        {
+           handle_container.current.style.left ="0px";
+          
         }
-        setCurrnum(num + 1);
-
-        change(num + 1);
-
-    }
-
-    const goprev = () => {
-        handle_container.current.style.transition = "0.4s ease-in";
-       
-        let num = currnum;
-       
-        if (num <= 0) {
-            Setbtn(true);
-            num = offsetchild;
- 
-            return;
+        else if(innerparent.right <outerparent.right)
+        {
+           
+            handle_container.current.style.left = `${-(innerparent.width - outerparent.width)}px`;
         }
-  
-        setCurrnum(num - 1);
-       
-        change(num - 1);
-    }
-    function change(num) {
-        setoffset(-170 * num);
     }
 
 
+
+   
     return <>
-        <div className="content-inside" ref={parent_handle_container}>
-            <button type="button" className="next-btn" onClick={gonext}><i className="fas fa-chevron-right"></i></button>
-            <button type="button" className="prev-btn" onClick={goprev}><i className="fas fa-chevron-left"></i></button>
-            <ul ref={handle_container} style={{ left: `${offset}px` }}
-                className="item-container">
+        <div className="content-inside" ref={parent_handle_container} onMouseDown={(e)=>
+        {
+           setoffsetx(e.nativeEvent.offsetX - handle_container.current.offsetLeft)
+            e.currentTarget.style.cursor = "grabbing";
+            e.preventDefault();
+            ispressed(true);
+            
+        }}
+        
+        onMouseEnter={e=>
+        {
+            e.currentTarget.style.cursor = "grab";
+        }}
+        onMouseUp={e=>
+        {
+            e.currentTarget.style.cursor = "grab";
+           
+        }}
+        
+        onMouseMove={(e)=>
+        {
+            mousemovefn(e)
+        }}
+        
+
+        >
+            <ul ref={handle_container} 
+                className="item-container" style={{width:`${items && (items.length *160)}px`}}>
                     {items && items.map((anime) => {
                         const { image_url, mal_id, title, episodes, score, favorites } = anime;
                         let color = "black";
@@ -141,9 +115,9 @@ export const Mulimgslider =  (prop)=>
                         }
                         return <li key={mal_id}>
 
-                            <div className="airing_container">
-                                {(image_url)?<img src={image_url} alt="" />:""}
-                                <div className="more">
+                            <div className="airing_container" >
+                                {(image_url)?<img src={image_url} ref={img} alt="" />:""}
+                                <div className="more" ref={moreinfo}>
                                     <h4>{(title)?(title.length>45)?title.substr(0,45)+"...":title:""}</h4>
                                     {favorites && <span><i className="fas fa-star"></i>: {favorites}</span>}
                                     {episodes && <span><i className="fas fa-tv"></i>: {episodes}</span>}
