91b310c10a18d35ed320b49ea7ed5677df09d8d3
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-08-22 18:40:03 +0530

    Bug fix: error E11000 duplicate key in mongodb

--- backend/Model/comment.js
+++ backend/Model/comment.js
@@ -2,82 +2,80 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 
 const commentsOn = new mongoose.Schema({
-    malid:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    maincomments:{
-        type:Number,
-        default:0
-    },
-    subcomments:{
-        type:Number,
-        default:0
-    },
-    comments:[
+  malid: {
+    type: String,
+    required: true,
+    unique: true,
+    sparse: true,
+  },
+  maincomments: {
+    type: Number,
+    default: 0,
+  },
+  subcomments: {
+    type: Number,
+    default: 0,
+  },
+  comments: [
+    {
+      body: String,
+      userID: String,
+      userProfileImg: String,
+      userName: String,
+      date: Date,
+      commentID: String,
+      likeCount: { type: Number, default: 0 },
+      dislikeCount: { type: Number, default: 0 },
+      replies: [
         {
-            body:String,
-            userID:String,
-            userProfileImg:String,
-            userName:String,
-            date: Date,
-            commentID:String,
-            likeCount:{type:Number,default:0},
-            dislikeCount:{type:Number,default:0},
-            replies:[{
-                body:String,
-                date:Date ,
-                replyID:Number,
-                userID:String
-            }]
-        }
-    ]
-    
-})
+          body: String,
+          date: Date,
+          replyID: Number,
+          userID: String,
+        },
+      ],
+    },
+  ],
+});
 
+commentsOn.methods.addComment = function (
+  comment,
+  userID,
+  userName,
+  userProfileImgPath
+) {
+  this.maincomments = this.maincomments + 1;
+  let commentsArr = this.comments;
+  commentsArr.push({
+    body: comment + "",
+    userID: userID + "",
+    userName,
+    userProfileImg: userProfileImgPath,
+    date: new Date(),
+    commentID: crypto.randomBytes(7).toString("hex"),
+  });
+  this.comments = commentsArr;
+  this.save();
 
-commentsOn.methods.addComment = function(comment,userID,userName,userProfileImgPath){
-
-    this.maincomments = this.maincomments +1;
-    let commentsArr = this.comments;
-    commentsArr.push({
-        body:comment+"",
-        userID:userID+"",
-        userName,
-        userProfileImg:userProfileImgPath,
-       date: new Date(),
-        commentID:crypto.randomBytes(7).toString("hex")
-    })
-    this.comments = commentsArr;
-    this.save();
-
-    return this._doc;
-
-}
+  return this._doc;
+};
 
 //* -- increase the like counter if increase is set to true, else remove the like
 
+commentsOn.methods.addLikeOrDislike = function (
+  __id,
+  increase,
+  whatToIncrease = "likeCount"
+) {
+  this.comments = this.comments.map((comment) => {
+    if (comment._id.valueOf() === __id) {
+      if (increase) {
+        return { ...comment, [whatToIncrease]: comment[whatToIncrease] + 1 };
+      }
+      return { ...comment, [whatToIncrease]: comment[whatToIncrease] - 1 };
+    }
+    return comment;
+  });
+};
 
-
-commentsOn.methods.addLikeOrDislike = function(__id,increase,whatToIncrease = "likeCount"){
-    
-    this.comments = this.comments.map((comment)=>{
-        
-        if(comment._id.valueOf() === __id){
-            if(increase){
-                
-                return {...comment,[whatToIncrease]:comment[whatToIncrease]+1}
-            }
-            return {...comment,[whatToIncrease]:comment[whatToIncrease]-1}
-           
-        }
-        return comment
-    })
-    
-
-    
-    
-}
-
-module.exports = mongoose.model("Comments",commentsOn);
+module.exports = mongoose.model("Comments", commentsOn);
