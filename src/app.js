2b621b205997537e96b80ab7a55fb193ae8d4073
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-08-26 18:14:49 +0530

    rotues are in a new container, navbar outside

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router ,Route ,Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "../src/Stats-Anime/header";
 import Anime from "../src/Stats-Anime/anime";
 import Topanime from "../src/Stats-Anime/topanime";
@@ -8,51 +8,55 @@ import Details from "../src/Stats-Anime/details";
 import Detailschar from "../src/Stats-Anime/details-char";
 import Listuser from "../src/Stats-Anime/list-user";
 import ScrollToTop from "../src/Stats-Anime/scrollTotop";
-import {Mainsection} from "./Stats-Anime/about";
-import {Errorpage} from "./Stats-Anime/error";
-import {SignupMain} from "./Stats-Anime/Signup"
-import {UserProfileMain} from "./Stats-Anime/user_profile"
-import {ReadOnlyProfileMain} from "./Stats-Anime/User_Profile_Read_Only"
+import { Mainsection } from "./Stats-Anime/about";
+import { Errorpage } from "./Stats-Anime/error";
+import { SignupMain } from "./Stats-Anime/Signup";
+import { UserProfileMain } from "./Stats-Anime/user_profile";
+import { ReadOnlyProfileMain } from "./Stats-Anime/User_Profile_Read_Only";
 
-function App()
-{
-    //* route starts
-    return <>
-    
-        
-        <Router>
-            
-            <ScrollToTop/>
-            <Header/>
-            <Routes>
-                <Route index path = "/" element = {<Anime/>}/>
-                   
-                <Route path = "/topanime" element = {<Topanime/>}/>
+function App() {
+  //* route starts
+  return (
+    <>
+      <Router>
+        <ScrollToTop />
+        <Header />
+        <div className="inner-root-container">
+          <Routes>
+            <Route index path="/" element={<Anime />} />
 
-                <Route path = "/topcharacters" element = {<Topcharacters/>}/>
-              
-                <Route path = "/about" element = {<Mainsection/>}/>
+            <Route path="/topanime" element={<Topanime />} />
 
-                <Route path="/user/:userID/view" element={<UserProfileMain/>}/>
-                <Route path="/user/:userID" element={<ReadOnlyProfileMain/>}/>
-                
-                 <Route path = "/useranimelist/:userID" element = {<Listuser header = "anime list" switch_item = "anime"/>}/>
+            <Route path="/topcharacters" element={<Topcharacters />} />
 
-                 <Route path = "/usercharacterlist/:userID" element = {<Listuser header = "character list" switch_item = "character"/>}/>
+            <Route path="/about" element={<Mainsection />} />
 
-                 <Route path = "anime/:id" element = {<Details/>} />
-                 <Route path = "character/:id" element = {<Detailschar/>}/>
+            <Route path="/user/:userID/view" element={<UserProfileMain />} />
+            <Route path="/user/:userID" element={<ReadOnlyProfileMain />} />
 
-                <Route path="userauth"  element ={<SignupMain/>} />
-                {/* routing to non-existing path */}
-                <Route path="*" element={<Errorpage/>} />
+            <Route
+              path="/useranimelist/:userID"
+              element={<Listuser header="anime list" switch_item="anime" />}
+            />
 
+            <Route
+              path="/usercharacterlist/:userID"
+              element={
+                <Listuser header="character list" switch_item="character" />
+              }
+            />
 
-            </Routes>
-        </Router>
-    
-       
+            <Route path="anime/:id" element={<Details />} />
+            <Route path="character/:id" element={<Detailschar />} />
+
+            <Route path="userauth" element={<SignupMain />} />
+            {/* routing to non-existing path */}
+            <Route path="*" element={<Errorpage />} />
+          </Routes>
+        </div>
+      </Router>
     </>
+  );
 }
 
 export default App;
