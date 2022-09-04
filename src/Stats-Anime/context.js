03994b768bebf481ffa4acef9bd287bc5d43f278
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-09-04 23:29:24 +0530

    removed some states

--- src/Stats-Anime/context.js
+++ src/Stats-Anime/context.js
@@ -1,64 +1,48 @@
 import React from "react";
 import react from "react";
-import { useState} from "react";
-
+import { useState } from "react";
 
 const Appcontext = react.createContext();
 
-const Approvider = ({children})=>
-{
+const Approvider = ({ children }) => {
+  const [ishamclick, setIshamclick] = useState(false);
+  const [editState, setEditState] = useState(false);
+  const [userData, setUserData] = useState();
+  const [userProfileDetails, setUserProfileDetails] = useState({});
 
-    const [ishamclick , setIshamclick] = useState(false);
-    const [loading,setLoading] = useState(false); //* trigger for the loading text
-    const [loadingtext,setLoadingtext] = useState(""); //* content of the loading text
-    const [editState, setEditState] = useState(false);
-    const [userData,setUserData] = useState()
-    const [userProfileDetails,setUserProfileDetails] = useState({})
+  const toggle = (bool) => {
+    setIshamclick(bool);
+  };
+  const changeEditState = (e) => {
+    e.preventDefault();
+    setEditState(!editState);
+  };
 
-    const toggle = (bool)=>
-    {
-        setIshamclick(bool);
-    }
-    const toggle_loading_state = (bool)=> //* handler for loading text
-    {
-        setLoading(bool);
-    }
-    const set_loading_text = (text)=>
-    {
-        setLoadingtext(text.toString());
-    }
+  const changeUserData = (user) => {
+    setUserData(user);
+  };
 
-    const changeEditState = (e)=>{
-        e.preventDefault()
-        setEditState(!editState);
-    }
+  //* for storing user's profile details between renders
+  const changeUserProfileDetails = (userData) => {
+    setUserProfileDetails(userData);
+  };
 
-    const changeUserData = (user)=>{
-        setUserData(user)
-    }
+  return (
+    <Appcontext.Provider
+      value={{
+        ishamclick,
+        toggle,
+        changeEditState,
+        editState,
+        changeUserData,
+        userData,
+        changeUserProfileDetails,
+        userProfileDetails,
+      }}
+    >
+      {children}
+    </Appcontext.Provider>
+  );
+};
 
-    //* for storing user's profile details between renders
-    const changeUserProfileDetails = (userData)=>{
-        setUserProfileDetails(userData);
-    }
-
-
-    return <Appcontext.Provider value = {
-        {ishamclick,
-            toggle,
-            loading,
-            loadingtext,
-            toggle_loading_state,
-            set_loading_text,
-            changeEditState,
-            editState,
-            changeUserData,
-            userData,
-            changeUserProfileDetails,
-            userProfileDetails
-        }
-        }>{children}</Appcontext.Provider>
-}
-
-
-export  {Approvider,Appcontext};
+export { Approvider, Appcontext };
