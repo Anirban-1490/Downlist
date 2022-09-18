bd7f54683680cb8a2c8edf64e3343f36a1e14286
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-09-18 17:16:38 +0530

    improved some jsx

--- src/Stats-Anime/Pages/About/index.js
+++ src/Stats-Anime/Pages/About/index.js
@@ -9,17 +9,16 @@ export const About = () => {
         <img src={downlistBackground} alt="" />
       </div>
       <p className="main-details">
-        Downlist is a <b>Personal Project</b> of mine which is a place for{" "}
-        <b>Anime Lovers</b>.
-        <br /> It's a place where you can view any <strong>
-          Upcoming
-        </strong>{" "}
-        anime or some of the <strong>Top</strong> ones. <br /> Want to checkout
-        any of your favourite anime ? don't worry we have <b>Live Search</b>{" "}
-        through which you can check details of any of your favourite anime.{" "}
+        Downlist is a place for <b>Anime Lovers</b>.
+        <br /> It's a place where you can view any <strong>Upcoming </strong>
+        anime or some of the <strong>Top</strong> ones. <br />
+        <br /> Want to checkout any of your favourite anime ? don't worry we
+        have <b>Live Search </b>
+        through which you can check details of any of your favourite anime.
+        <br />
         <br />
         Want to checkout out about your favourite <strong>Character</strong>?
-        the character section got information about <strong>Top</strong>{" "}
+        the character section got information about <strong>Top </strong>
         characters and much more....
       </p>
       <h3>
