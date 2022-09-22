2017cac2515f8ce977df94b41ab3d5c6ecbef10d
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-09-22 17:08:53 +0530

    custom hook to fetch user's profile

---
+++ src/Stats-Anime/Hooks/useProfile.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Appcontext } from "../context";
+
+import { useContext } from "react";
+
+export const useProfile = (path, userID) => {
+  const { changeUserProfileDetails } = useContext(Appcontext);
+  const token = localStorage.getItem("token") || "anonymous";
+  async function fetchUserProfile() {
+    return (await axios.get(`${path.domain}user/${userID}/profile/view`)).data;
+  }
+  const { data } = useQuery(["profile", token], fetchUserProfile, {
+    refetchOnWindowFocus: false,
+    cacheTime: 0,
+    enabled: !!userID,
+    notifyOnChangeProps: ["data"],
+
+    onSettled: (data, err) => {
+      console.log("re");
+      if (err) return console.log(err);
+
+      changeUserProfileDetails(data); //*store the data in the context
+      return data.user.image;
+    },
+  });
+
+  return data;
+};
