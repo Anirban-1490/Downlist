8f5e8f0403b58e05c749205c995e9940a276e257
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-09-22 17:14:24 +0530

    revised logic using more custom hook

--- src/Stats-Anime/Components/Navbar/index.js
+++ src/Stats-Anime/Components/Navbar/index.js
@@ -1,23 +1,24 @@
 import React, { useCallback, useMemo } from "react";
 import "./header-style.css";
-import { useState, useRef, useContext, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useState, useContext } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Appcontext } from "../../context";
-import axios from "axios";
-import { useQuery } from "react-query";
 import { useAuth } from "../../Feature/Authorize/Authorize";
 import { path } from "../../../server-path";
 import { Navbar } from "./Components/Navbar";
 import { useWindowResize } from "../../Hooks/useWindowResize";
 import { MobileNavbar } from "./Components/MobileNavbar";
+import { useProfile } from "../../Hooks/useProfile";
 
 export function ParentNavbar() {
   const [isHamClicked, setHamClicked] = useState(false);
   const innerWidth = useWindowResize();
-  console.log("hey");
+
   const userData = useAuth(true); //* custom hook for checking if user logged in or not
   const { pathname } = useLocation(); //* used to rerender the component if we hit back button to come here
-  const { changeUserData, changeUserProfileDetails } = useContext(Appcontext);
+  const navigate = useNavigate();
+  const { changeUserData } = useContext(Appcontext);
+
   changeUserData(userData);
 
   const isSmallScreenWidth = innerWidth > 740 ? false : true;
@@ -27,43 +28,32 @@ export function ParentNavbar() {
     }
   }, [isSmallScreenWidth]);
 
-  const token = localStorage.getItem("token");
-  async function fetchUserProfile() {
-    return (
-      await axios.get(`${path.domain}user/${userData.userID}/profile/view`)
-    ).data;
-  }
-  const { data } = useQuery(["profile", token], fetchUserProfile, {
-    refetchOnWindowFocus: false,
-    staleTime: Infinity,
-    cacheTime: 0,
-    enabled: !!userData,
-    onSettled: (data, err) => {
-      if (err) return console.log(err);
-
-      changeUserProfileDetails(data); //*store the data in the context
-      return data.data.user.image;
-    },
-  });
+  const userProfileData = useProfile(path, userData?.userID);
 
   const signoutHandler = useCallback(() => {
     localStorage.removeItem("token");
-    if (window.location.pathname !== "/") {
-      window.location.replace("/");
+    if (pathname !== "/") {
+      navigate("/", { replace: true });
 
       return;
     }
     window.location.reload();
-  }, []);
+  }, [navigate, pathname]);
 
   const props = useMemo(
     () => ({
       signoutHandler,
       setHamClicked,
       isHamClicked,
-      data: userData && { ...userData, ...data?.user },
+      data: userData && { ...userData, ...userProfileData?.user },
     }),
-    [setHamClicked, isHamClicked, signoutHandler, userData, data?.user]
+    [
+      setHamClicked,
+      isHamClicked,
+      signoutHandler,
+      userData,
+      userProfileData?.user,
+    ]
   );
 
   return (
