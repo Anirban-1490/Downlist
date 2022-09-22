e79041124b3c628dd4d4f1a9cf49fc4546110660
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-09-22 17:13:02 +0530

    unique id added to authorize request url

--- src/Stats-Anime/Feature/Authorize/Authorize.js
+++ src/Stats-Anime/Feature/Authorize/Authorize.js
@@ -1,29 +1,35 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { v4 as generateUUID } from "uuid";
 import { path } from "../../../server-path";
 
 export const useAuth = (isenabled, redirect = false) => {
   //? custom hook to check if user is logged in or not when routing to any page
 
   const navigate = useNavigate();
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token") || "anonymous";
 
-  const authorizeUser = () => {
-    return axios
-      .get(`${path.domain}api/v1/auth/authorize`, {
+  const authorizeUser = async () => {
+    const uniqueID = generateUUID();
+
+    return (
+      await axios.get(`${path.domain}api/v1/auth/authorize?reqID=${uniqueID}`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then((res) => res.data);
+    ).data;
   };
 
   const { data } = useQuery(["user", token], () => authorizeUser(), {
     refetchOnWindowFocus: false,
+    retry: false,
+    notifyOnChangeProps: ["data"],
     onSettled: (data, err) => {
       if (err) {
-        return console.log(err?.response?.data);
+        console.log(err?.response?.data);
+        return;
       }
 
       //*this will remove the current route(the signup route) from history stack so user's can't go back to it. and also redirect the user to the home page
