b677ccf7ddb7d96923176fd7818b75723d101b82
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-09-24 20:22:02 +0530

    scroll to top when refreshing page

--- src/Stats-Anime/Components/ScrollToTop/ScrollToTop.jsx
+++ src/Stats-Anime/Components/ScrollToTop/ScrollToTop.jsx
@@ -4,8 +4,13 @@ import { useLocation } from "react-router-dom";
 export function ScrollToTop() {
   const { pathname } = useLocation();
 
+  window.addEventListener("unload", () => {
+    window.scrollTo(0, 0);
+  });
+
   useEffect(() => {
     window.scrollTo(0, 0);
+    console.log("ran");
   }, [pathname]);
 
   return null;
