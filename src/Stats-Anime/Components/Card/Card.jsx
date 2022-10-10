bf904f3640fdddfc0dd38fce111ef36c7f5b49ac
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-10-10 22:20:39 +0530

    anime type added

--- src/Stats-Anime/Components/Card/Card.jsx
+++ src/Stats-Anime/Components/Card/Card.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
 import "./CardStyle.scss";
 
-export const Card = ({ mal_id, path, image_url, mainTitle, subTitle }) => {
+export const Card = ({
+  mal_id,
+  path,
+  image_url,
+  mainTitle,
+  subTitle,
+  animeType,
+}) => {
   return (
     <>
       <Link to={path ? path + `/${mal_id}` : ``} className="cards" key={mal_id}>
         {image_url ? <img src={image_url} alt="" /> : ""}
+        {animeType && <h3 className="type">{animeType}</h3>}
 
         <div className="details-char">
           <h5
