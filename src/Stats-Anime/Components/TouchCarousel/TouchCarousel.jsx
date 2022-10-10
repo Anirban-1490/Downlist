1836d295c8ce4b46ddcdc5ead65c6d486a29632d
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-10-10 22:06:08 +0530

    usage of the custom card component

--- src/Stats-Anime/Components/TouchCarousel/TouchCarousel.jsx
+++ src/Stats-Anime/Components/TouchCarousel/TouchCarousel.jsx
@@ -2,52 +2,43 @@ import React from "react";
 
 import "../../Pages/Top/Styles/topanimestyle.css";
 import { Link } from "react-router-dom";
-import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
-import "swiper/swiper.min.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/swiper-bundle.css";
+// import "swiper/css/navigation";
 import default_img from "../../logo/default-placeholder.png";
+import { Card } from "../Card/Card";
 
 //* image slider component
 
 export const TouchCarousel = ({ items, switch_details }) => {
   return (
     <>
-      <Swiper className="swiper" spaceBetween={8} slidesPerView={"auto"}>
+      <Swiper
+        className="swiper"
+        spaceBetween={12}
+        slidesPerView={"auto"}
+        navigation={true}
+        modules={[Navigation]}
+      >
         {items &&
           items.map((anime) => {
             const {
-              image_url,
               mal_id,
               title,
               name,
               images: {
-                jpg: { image_url: newImageUrl },
+                jpg: { image_url },
               },
               type,
             } = anime;
+            const mainTitle = title || name;
+            const animeType = type || ``;
+            const path = switch_details === "anime" ? `/anime` : `/character`;
+            const props = { mal_id, image_url, path, mainTitle, animeType };
             return (
               <SwiperSlide className="swiper-slide" key={mal_id}>
-                <div className="airing_container">
-                  <img
-                    src={image_url || newImageUrl}
-                    onError={(currentElement) =>
-                      (currentElement.target.src = default_img)
-                    }
-                    alt=""
-                  />
-                  <div className="more">
-                    {type && <h5 className="type">{type}</h5>}
-                    {(title || name) && <h3>{title || name}</h3>}
-                    <div className="see-more-container">
-                      <Link
-                        className="see-more"
-                        to={switch_details ? switch_details + `/${mal_id}` : ""}
-                      >
-                        See more
-                      </Link>
-                      <ion-icon name="arrow-forward-outline"></ion-icon>
-                    </div>
-                  </div>
-                </div>
+                <Card {...props} />
               </SwiperSlide>
             );
           })}
