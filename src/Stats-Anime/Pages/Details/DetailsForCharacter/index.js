a7d1bbd9442d26d639308dd863b2b120af661aa1
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-10-22 19:07:35 +0530

    updated to jikan v4

--- src/Stats-Anime/Pages/Details/DetailsForCharacter/index.js
+++ src/Stats-Anime/Pages/Details/DetailsForCharacter/index.js
@@ -12,16 +12,34 @@ import { VoiceActors } from "./Components/VoiceActors";
 //* component for character details
 
 export function CharacetrDetailsMain() {
-  const { id } = useParams();
-  const malid = id;
+  const { id: mailId } = useParams();
 
-  const getCharacterDetails = (url) => axios.get(url).then((res) => res.data);
+  const getCharacterDetails = async (url) => {
+    return await axios.get(url).then(({ data: { data } }) => data);
+  };
 
   const { data, isLoading, isError } = useQuery(
     "char_details",
-    () => getCharacterDetails(`https://api.jikan.moe/v3/character/${malid}`),
+    () =>
+      getCharacterDetails(`https://api.jikan.moe/v4/characters/${mailId}/full`),
     { cacheTime: 0, refetchOnWindowFocus: false }
   );
+
+  const characterDetails = {
+    title: data?.name_kanji,
+    title_englidh: data?.name,
+    favorites: data?.favorites,
+    synopsis: data?.about,
+    images: data?.images,
+  };
+
+  const details = {
+    details: characterDetails,
+    animegenres: null,
+    stats: null,
+    char: null,
+    mailId,
+  };
 
   return (
     <>
@@ -32,17 +50,7 @@ export function CharacetrDetailsMain() {
       ) : (
         <div className="container1" style={{ height: "auto" }}>
           <CoreDetails
-            details={{
-              animedetails: data,
-              animegenres: null,
-              stats: null,
-              char: null,
-              malid,
-            }}
-            fav={data?.member_favorites}
-            about={data?.about}
-            name={data?.name}
-            name_kenji={data?.name_kanji}
+            {...details}
             switch_item="character"
             switch_path="topcharacters"
           />
@@ -59,7 +67,7 @@ export function CharacetrDetailsMain() {
           >
             Anime Appearances
           </h4>
-          <AnimeAppearances appearances={data?.animeography} />
+          <AnimeAppearances appearances={data?.anime} />
           <h4
             style={{
               color: "white",
@@ -73,7 +81,7 @@ export function CharacetrDetailsMain() {
           >
             Voice Actors
           </h4>
-          <VoiceActors voiceactors={data?.voice_actors} />
+          <VoiceActors voiceactors={data?.voices} />
         </div>
       )}
     </>
