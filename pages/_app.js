586a9f540ce22c5a107f58019e1a45444e29502a
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-04 21:37:15 +0530

    fixzes warning

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ function MyApp({ Component, pageProps }) {
           content="uplist , anime , manga , characters , anime characters , myanimelist , animelist , downlist"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link rel="apple-touch-icon" href="./DownlistLogoIcon.png" />
 
         <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
