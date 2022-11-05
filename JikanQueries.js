b4d5f96aa416780f3964ac56af99f2df3e68e219
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-05 17:08:35 +0530

    new query added

--- JikanQueries.js
+++ JikanQueries.js
@@ -5,6 +5,7 @@ export const jikanQueries = (key, params) => {
     details: `https://api.jikan.moe/v4/anime/${params}/full`,
     people_reaction: `https://api.jikan.moe/v4/anime/${params}/statistics`,
     characters: `https://api.jikan.moe/v4/anime/${params}/characters`,
+    char_details: `https://api.jikan.moe/v4/characters/${params}/full`,
   };
 
   return axios.get(queries[key]).then(({ data: { data } }) => data);
