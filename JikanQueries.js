ab235a1f46caca3384ab1e57e85d43a9b60e6452
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-07 17:15:14 +0530

    new queries added

--- JikanQueries.js
+++ JikanQueries.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-export const jikanQueries = (key, params) => {
-  const queries = {
-    details: `https://api.jikan.moe/v4/anime/${params}/full`,
-    people_reaction: `https://api.jikan.moe/v4/anime/${params}/statistics`,
-    characters: `https://api.jikan.moe/v4/anime/${params}/characters`,
-    char_details: `https://api.jikan.moe/v4/characters/${params}/full`,
-  };
+export const jikanQueries = async (key, ...params) => {
+  const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
+  const queries = {
+    details: `https://api.jikan.moe/v4/anime/${params[0]}/full`,
+    people_reaction: `https://api.jikan.moe/v4/anime/${params[0]}/statistics`,
+    characters: `https://api.jikan.moe/v4/anime/${params[0]}/characters`,
+    char_details: `https://api.jikan.moe/v4/characters/${params[0]}/full`,
+    top_anime: `https://api.jikan.moe/v4/top/anime?filter=${params[0]}&page=${params[1]}&limit=${params[2]}`,
+  };
+  await delay();
   return axios.get(queries[key]).then(({ data: { data } }) => data);
 };
