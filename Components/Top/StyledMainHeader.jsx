c7c8928c31dcc8281eac40e70cb77e24efeef657
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-08 23:44:36 +0530

    new moduler style for the StyledMainHeader

--- Components/Top/StyledMainHeader.jsx
+++ Components/Top/StyledMainHeader.jsx
@@ -1,21 +1,18 @@
+import mainHeaderStyle from "Components/Top/style/MainHeader.module.scss";
+
 export function StyledMainHeader(prop) {
   const { text, isanimateable, subtext } = prop.content;
 
   return (
     <>
-      <div className="head-main-container">
-        <div className="inner-content">
-          <span>
-            {isanimateable && <div className="ball"></div>}
-            <h1>
-              {text[0]}
-              {isanimateable && <span className="anim-letter">{text[1]}</span>}
-              {text[2]}
-            </h1>
-          </span>
-          <span>
-            <h1>{text[3]}</h1>
-          </span>
+      <div className={mainHeaderStyle["head-main-container"]}>
+        <div className={mainHeaderStyle["inner-content"]}>
+          <h1>
+            {text[0]}
+            {isanimateable && <div className={mainHeaderStyle["ball"]}></div>}
+          </h1>
+
+          <h1>{text[1]}</h1>
         </div>
         <h3>{subtext}</h3>
       </div>
