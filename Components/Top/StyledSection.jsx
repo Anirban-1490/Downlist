447e38952734b050a6395b95db484d33be109fbf
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-09 15:47:14 +0530

    moduler style for the StyledSection component

--- Components/Top/StyledSection.jsx
+++ Components/Top/StyledSection.jsx
@@ -1,14 +1,14 @@
 import { TouchCarousel } from "Components/Global/TouchCarousel/TouchCarousel";
 import react from "react";
+import mainSectionStyle from "Components/Top/style/StyledMainSection.module.scss";
 
 export const StyledSection = react.memo((prop) => {
-  const { data, switch_details, text_, className } = prop;
+  const { data, switch_details, text_, headerColor } = prop;
 
-  console.log(data);
   return (
     <>
-      <div className={className}>
-        <h2>{text_}</h2>
+      <div className={mainSectionStyle["section-wrapper"]}>
+        <h2 style={{ color: headerColor }}>{text_}</h2>
         <TouchCarousel items={data} switch_details={switch_details} />
       </div>
     </>
