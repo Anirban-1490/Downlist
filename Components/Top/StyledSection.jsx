1062926d200151dbc3d789afc5080a84144b85f3
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-11 23:32:16 +0530

    added a nhew sub header

--- Components/Top/StyledSection.jsx
+++ Components/Top/StyledSection.jsx
@@ -2,15 +2,26 @@ import { TouchCarousel } from "Components/Global/TouchCarousel/TouchCarousel";
 import react from "react";
 import mainSectionStyle from "Components/Top/style/StyledMainSection.module.scss";
 
-export const StyledSection = react.memo((prop) => {
-  const { data, switch_details, text_, headerColor } = prop;
+export const StyledSection = react.memo(
+  ({
+    data,
+    switch_details,
+    text_,
+    headerColor,
+    subHeadingText,
+    subHeaderColor = "#ffffffa7",
+  }) => {
+    return (
+      <>
+        <div className={mainSectionStyle["section-wrapper"]}>
+          <h2 style={{ color: headerColor }}>
+            {text_}
+            <h4 style={{ color: subHeaderColor }}>{subHeadingText}</h4>
+          </h2>
 
-  return (
-    <>
-      <div className={mainSectionStyle["section-wrapper"]}>
-        <h2 style={{ color: headerColor }}>{text_}</h2>
-        <TouchCarousel items={data} switch_details={switch_details} />
-      </div>
-    </>
-  );
-});
+          <TouchCarousel items={data} switch_details={switch_details} />
+        </div>
+      </>
+    );
+  }
+);
