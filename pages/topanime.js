0023e9afb100c4959a011344bc9a216138b9298e
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-13 18:49:33 +0530

    anime characters markup

--- pages/topanime.js
+++ pages/topanime.js
@@ -69,6 +69,11 @@ function TopAnime() {
               subHeadingText={"Latest toppers"}
             />
             <div
+              className={`${topStyle["anime-character"]} ${topStyle["character-1"]}`}
+            >
+              <img src={"/AnimeCharacter1.png"} alt="" />
+            </div>
+            <div
               className={`${topStyle["custom-shape-divider-bottom-1668163489"]} ${topStyle["custom-shape-divider"]}`}
             >
               <svg
@@ -79,7 +84,7 @@ function TopAnime() {
               >
                 <path
                   d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                  style={{ fill: "#191934b5" }}
+                  className={topStyle["shape-path-1"]}
                 ></path>
               </svg>
             </div>
@@ -103,9 +108,14 @@ function TopAnime() {
               >
                 <path
                   d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-                  style={{ fill: "#271f35" }}
+                  className={topStyle["shape-path-2"]}
                 ></path>
               </svg>
+            </div>
+            <div
+              className={`${topStyle["anime-character"]} ${topStyle["character-2"]}`}
+            >
+              <img src={"/AnimeCharacter2.png"} alt="" />
             </div>
           </div>
           <div className={`${topStyle["section"]} ${topStyle["section-3"]}`}>
