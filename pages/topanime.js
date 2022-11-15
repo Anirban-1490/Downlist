44c61a983d7a545df14de1cfc1ce39c33c40a176
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-11-15 18:32:41 +0530

    added new section devider

--- pages/topanime.js
+++ pages/topanime.js
@@ -126,6 +126,21 @@ function TopAnime() {
               headerColor={"#ec95bb"}
               subHeadingText={"All time favorites"}
             />
+            <div
+              className={`${topStyle["custom-shape-divider-bottom-1668509441"]} ${topStyle["custom-shape-divider"]}`}
+            >
+              <svg
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
+                  className={topStyle["shape-path-3"]}
+                ></path>
+              </svg>
+            </div>
           </div>
           <div
             className={`${topStyle["section"]} ${topStyle["section-4"]}`}
