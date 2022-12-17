92b802f25bddeff8f2b278d6abce7ffacfcab883
Anirban <anirbanpratihar45@gmail.com>
2022-12-17 20:31:55 +0530

    new props added for custom styles

--- Components/Global/NoItemFound/NoItemFound.jsx
+++ Components/Global/NoItemFound/NoItemFound.jsx
@@ -1,20 +1,30 @@
+import { memo } from "react";
 import noitemStyle from "./NoItem.module.scss";
 
-export const NoItem = ({ content, textColor }) => {
-  return (
-    <p
-      className={noitemStyle["no-item-main"]}
-      style={{ color: textColor || "#6a6969" }}
-    >
-      {content || "Nothing to show here"}
-    </p>
-  );
-};
+export const NoItem = memo(({ content, textColor, customStyleText }) => {
+    return (
+        <p
+            className={noitemStyle["no-item-main"]}
+            style={{ color: textColor || "#6a6969", ...customStyleText }}>
+            {content || "Nothing to show here"}
+        </p>
+    );
+});
 
-export const NoItemContiner = ({ content, textColor }) => {
-  return (
-    <div className={noitemStyle["no-item-container"]}>
-      {<NoItem content={content} textColor={textColor} />}
-    </div>
-  );
-};
+export const NoItemContiner = memo(
+    ({ content, textColor, customContainerStyle, customStyleText }) => {
+        return (
+            <div
+                className={noitemStyle["no-item-container"]}
+                style={customContainerStyle}>
+                {
+                    <NoItem
+                        content={content}
+                        textColor={textColor}
+                        customStyleText={customStyleText}
+                    />
+                }
+            </div>
+        );
+    }
+);
