fb0536b50c6b7272336698cdd3f4dab233e3310b
Anirban <anirbanpratihar45@gmail.com>
2022-12-23 21:12:20 +0530

    return value change from useProfile hook

--- Components/Global/Navbar/index.js
+++ Components/Global/Navbar/index.js
@@ -11,66 +11,66 @@ import { useProfile } from "Hooks/useProfile";
 import { Navbar } from "./Components/Navbar";
 
 export function ParentNavbar() {
-  const router = useRouter();
-  const { pathname, asPath } = router;
-  const [isHamClicked, setHamClicked] = useState(false);
-  const innerWidth = useWindowResize();
-  const [userData, _] = useAuth(true);
+    const router = useRouter();
+    const { pathname, asPath } = router;
+    const [isHamClicked, setHamClicked] = useState(false);
+    const innerWidth = useWindowResize();
+    const [userData, _] = useAuth(true);
 
-  //* custom hook for checking if user logged in or not
-  //* used to rerender the component if we hit back button to come here
+    //* custom hook for checking if user logged in or not
+    //* used to rerender the component if we hit back button to come here
 
-  const isSmallScreenWidth = innerWidth > 740 ? false : true;
+    const isSmallScreenWidth = innerWidth > 740 ? false : true;
 
-  useMemo(() => {
-    if (!isSmallScreenWidth) {
-      setHamClicked(false);
-    }
-  }, [isSmallScreenWidth]);
+    useMemo(() => {
+        if (!isSmallScreenWidth) {
+            setHamClicked(false);
+        }
+    }, [isSmallScreenWidth]);
 
-  const userProfileData = useProfile(path, userData?.userID);
+    const [userProfileData] = useProfile(path, userData?.userID);
 
-  const signoutHandler = useCallback(() => {
-    localStorage.removeItem("token");
+    const signoutHandler = useCallback(() => {
+        localStorage.removeItem("token");
 
-    window.location.href = "/";
-  }, [router, pathname]);
+        window.location.href = "/";
+    }, [router, pathname]);
 
-  const props = useMemo(
-    () => ({
-      signoutHandler,
-      setHamClicked,
-      isHamClicked,
-      data: userData && { ...userData, ...userProfileData?.user },
-    }),
-    [
-      setHamClicked,
-      isHamClicked,
-      signoutHandler,
-      userData,
-      userProfileData?.user,
-    ]
-  );
+    const props = useMemo(
+        () => ({
+            signoutHandler,
+            setHamClicked,
+            isHamClicked,
+            data: userData && { ...userData, ...userProfileData?.user },
+        }),
+        [
+            setHamClicked,
+            isHamClicked,
+            signoutHandler,
+            userData,
+            userProfileData?.user,
+        ]
+    );
 
-  return (
-    <>
-      {isSmallScreenWidth === true ? <MobileNavbar {...props} /> : ""}
+    return (
+        <>
+            {isSmallScreenWidth === true ? <MobileNavbar {...props} /> : ""}
 
-      {useMemo(() => {
-        return (
-          <>
-            {
-              <Navbar
-                {...{
-                  ...props,
-                  isSmallScreenWidth,
-                  path: pathname,
-                }}
-              />
-            }
-          </>
-        );
-      }, [isSmallScreenWidth, props, pathname])}
-    </>
-  );
+            {useMemo(() => {
+                return (
+                    <>
+                        {
+                            <Navbar
+                                {...{
+                                    ...props,
+                                    isSmallScreenWidth,
+                                    path: pathname,
+                                }}
+                            />
+                        }
+                    </>
+                );
+            }, [isSmallScreenWidth, props, pathname])}
+        </>
+    );
 }
--- Hooks/useProfile.js
+++ Hooks/useProfile.js
@@ -5,29 +5,25 @@ import { Appcontext } from "context";
 import { useContext } from "react";
 
 export const useProfile = (path, userID) => {
-  const { changeUserProfileDetails } = useContext(Appcontext);
-  const token =
-    typeof localStorage !== "undefined"
-      ? localStorage.getItem("token")
-      : "anonymous";
+    const { changeUserProfileDetails } = useContext(Appcontext);
 
-  async function fetchUserProfile() {
-    return (await axios.get(`${path.domain}user/${userID}/profile/view`)).data;
-  }
-  const { data } = useQuery(["profile", token], fetchUserProfile, {
-    refetchOnWindowFocus: false,
-    cacheTime: 0,
-    enabled: !!userID,
-    notifyOnChangeProps: ["data"],
+    async function fetchUserProfile() {
+        return (await axios.get(`${path.domain}user/${userID}/profile/view`))
+            .data;
+    }
+    const { data, isError } = useQuery(["profile", userID], fetchUserProfile, {
+        refetchOnWindowFocus: false,
+        cacheTime: 0,
+        enabled: !!userID,
+        notifyOnChangeProps: ["data"],
 
-    onSettled: (data, err) => {
-      console.log("re");
-      if (err) return console.log(err);
+        onSettled: (data, err) => {
+            if (err) return console.log(err);
 
-      changeUserProfileDetails(data); //*store the data in the context
-      return data.user.image;
-    },
-  });
+            changeUserProfileDetails(data); //*store the data in the context
+            return data.user.image;
+        },
+    });
 
-  return data;
+    return [data, isError];
 };
