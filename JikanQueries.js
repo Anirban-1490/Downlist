b0498a365e754480d9bf722cbd641a6b0670b1a8
Anirban <anirbanpratihar45@gmail.com>
2022-12-30 23:26:26 +0530

    increased time for delay

--- JikanQueries.js
+++ JikanQueries.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 export const jikanQueries = async (key, ...params) => {
-  const delay = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+    const delay = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 
-  const queries = {
-    details: `https://api.jikan.moe/v4/anime/${params[0]}/full`,
-    people_reaction: `https://api.jikan.moe/v4/anime/${params[0]}/statistics`,
-    characters: `https://api.jikan.moe/v4/anime/${params[0]}/characters`,
-    char_details: `https://api.jikan.moe/v4/characters/${params[0]}/full`,
-    top_anime: `https://api.jikan.moe/v4/top/anime?filter=${params[0]}&page=${params[1]}&limit=${params[2]}`,
-    top_char: `https://api.jikan.moe/v4/top/characters`,
-  };
-  await delay();
-  return axios.get(queries[key]).then(({ data: { data } }) => data);
+    const queries = {
+        details: `https://api.jikan.moe/v4/anime/${params[0]}/full`,
+        people_reaction: `https://api.jikan.moe/v4/anime/${params[0]}/statistics`,
+        characters: `https://api.jikan.moe/v4/anime/${params[0]}/characters`,
+        char_details: `https://api.jikan.moe/v4/characters/${params[0]}/full`,
+        top_anime: `https://api.jikan.moe/v4/top/anime?filter=${params[0]}&page=${params[1]}&limit=${params[2]}`,
+        top_char: `https://api.jikan.moe/v4/top/characters`,
+    };
+    await delay();
+    return axios.get(queries[key]).then(({ data: { data } }) => data);
 };
