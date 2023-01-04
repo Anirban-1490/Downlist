348fc85d775806645110a51dbd94ca2d54fb4911
Anirban <anirbanpratihar45@gmail.com>
2023-01-04 21:48:28 +0530

    added custom skeleton loader component

--- Components/Top/StyledSection.jsx
+++ Components/Top/StyledSection.jsx
@@ -1,27 +1,37 @@
 import { TouchCarousel } from "Components/Global/TouchCarousel/TouchCarousel";
 import react from "react";
 import mainSectionStyle from "Components/Top/style/StyledMainSection.module.scss";
+import { SkeletonLoaderMulti } from "Components/Global/SkeletionLoader/SkeletionLoaderMulti";
 
 export const StyledSection = react.memo(
-  ({
-    data,
-    switch_details,
-    text_,
-    headerColor,
-    subHeadingText,
-    subHeaderColor = "#ffffffa7",
-  }) => {
-    return (
-      <>
-        <div className={mainSectionStyle["section-wrapper"]}>
-          <h2 style={{ color: headerColor }}>
-            {text_}
-            <h4 style={{ color: subHeaderColor }}>{subHeadingText}</h4>
-          </h2>
+    ({
+        data,
+        switch_details,
+        text_,
+        headerColor,
+        subHeadingText,
+        subHeaderColor = "#ffffffa7",
+    }) => {
+        return (
+            <>
+                <div className={mainSectionStyle["section-wrapper"]}>
+                    <h2 style={{ color: headerColor }}>
+                        {text_}
+                        <h4 style={{ color: subHeaderColor }}>
+                            {subHeadingText}
+                        </h4>
+                    </h2>
 
-          <TouchCarousel items={data} switch_details={switch_details} />
-        </div>
-      </>
-    );
-  }
+                    {!data ? (
+                        <SkeletonLoaderMulti />
+                    ) : (
+                        <TouchCarousel
+                            items={data}
+                            switch_details={switch_details}
+                        />
+                    )}
+                </div>
+            </>
+        );
+    }
 );
