811cdc62fe0e07ea21b345e1462d209522a4e62f
Anirban <anirbanpratihar45@gmail.com>
2023-01-07 21:45:55 +0530

    taking more props for flex-column layout, width and height

--- Components/Global/SkeletionLoader/SkeletionLoaderMulti.jsx
+++ Components/Global/SkeletionLoader/SkeletionLoaderMulti.jsx
@@ -2,23 +2,33 @@ import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 import skeletonStyle from "Components/Global/SkeletionLoader/Skeletion.module.scss";
 
-export function SkeletonLoaderMulti() {
+export function SkeletonLoaderMulti({
+    skeletonCount = 8,
+    skeletonWidth = "11em",
+    skeletonHeight = "14em",
+    columnNormal = false,
+    columnMobile = false,
+}) {
+    const classForColumnNormal = columnNormal
+        ? "column-normal"
+        : columnMobile
+        ? "column-mobile"
+        : "";
+
     return (
         <>
             <div className={skeletonStyle["skeletion-wrapper"]}>
                 {
                     <SkeletonTheme
                         inline={true}
-                        width={"11em"}
-                        height={"14em"}
+                        width={skeletonWidth}
+                        height={skeletonHeight}
                         baseColor={"#302c2c"}
                         highlightColor={"#636161"}>
                         <Skeleton
-                            count={8}
-                            containerClassName={
-                                skeletonStyle["skeleton-wrapper-inner"]
-                            }
-                            className={skeletonStyle["skeleton-custom"]}
+                            count={skeletonCount}
+                            containerClassName={`${skeletonStyle["skeleton-wrapper-inner"]} ${skeletonStyle[classForColumnNormal]}`}
+                            className={`${skeletonStyle["skeleton-custom"]} ${skeletonStyle[classForColumnNormal]}`}
                         />
                     </SkeletonTheme>
                 }
