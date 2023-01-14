e6283200d0e42365f5866799a6d43c6c43037448
Anirban <anirbanpratihar45@gmail.com>
2023-01-14 18:04:09 +0530

    Javascript Map to hold geners as key and their malid as value

---
+++ genres.js
@@ -0,0 +1,22 @@
+export function getAllGenres() {
+    return new Map([
+        ["action", 1],
+        ["adventure", 2],
+        ["comedy", 4],
+        ["drama", 8],
+        ["fantasy", 10],
+        ["horror", 14],
+        ["mystery", 7],
+        ["romance", 22],
+        ["sci_fi", 24],
+        ["slice_of_life", 36],
+        ["sports", 30],
+        ["supernatural", 37],
+        ["detective", 39],
+        ["educational", 56],
+        ["historical", 13],
+        ["isekai", 62],
+        ["military", 38],
+        ["kids", 15],
+    ]);
+}
