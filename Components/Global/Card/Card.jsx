5ae72ae63e6f423e7bdf566810fa3c9926ba6733
Anirban <anirbanpratihar45@gmail.com>
2023-01-23 12:59:19 +0530

    tooltip on hover over a Card

--- Components/Global/Card/Card.jsx
+++ Components/Global/Card/Card.jsx
@@ -4,34 +4,36 @@ import { useEffect, useState } from "react";
 import cardStyle from "./Card.module.scss";
 
 export const Card = ({
-  mal_id,
-  path,
-  image_url,
-  mainTitle,
-  subTitle,
-  animeType,
+    mal_id,
+    path,
+    image_url,
+    mainTitle,
+    subTitle,
+    animeType,
 }) => {
-  const [token, setToken] = useState();
-  useEffect(() => {
-    setToken(getUserToken());
-  }, []);
-  return (
-    <>
-      <Link href={path ? path + `/${mal_id}` : `/`}>
-        <a className={cardStyle.cards} key={mal_id}>
-          <img
-            src={image_url}
-            alt=""
-            onError={(e) => (e.currentTarget.src = "/default-placeholder.png")}
-          />
-          {animeType && <h3 className={cardStyle.type}>{animeType}</h3>}
+    const [token, setToken] = useState();
 
-          <div className={cardStyle["details"]}>
-            <h5>{mainTitle}</h5>
-            {subTitle && <h5>{subTitle}</h5>}
-          </div>
-        </a>
-      </Link>
-    </>
-  );
+    return (
+        <>
+            <Link href={path ? path + `/${mal_id}` : `/`}>
+                <a className={cardStyle.cards} key={mal_id} title={mainTitle}>
+                    <img
+                        src={image_url}
+                        alt={`${mainTitle}-image`}
+                        onError={(e) =>
+                            (e.currentTarget.src = "/default-placeholder.png")
+                        }
+                    />
+                    {animeType && (
+                        <h3 className={cardStyle.type}>{animeType}</h3>
+                    )}
+
+                    <div className={cardStyle["details"]}>
+                        <h5>{mainTitle}</h5>
+                        {subTitle && <h5>{subTitle}</h5>}
+                    </div>
+                </a>
+            </Link>
+        </>
+    );
 };
