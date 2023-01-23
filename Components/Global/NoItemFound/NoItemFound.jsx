eef82feaa8bc0de2df2ae11c168ec255118786a0
Anirban <anirbanpratihar45@gmail.com>
2023-01-23 13:07:49 +0530

    added a retry button to refetch a network request incase of any Error happens

--- Components/Global/NoItemFound/NoItemFound.jsx
+++ Components/Global/NoItemFound/NoItemFound.jsx
@@ -1,29 +1,45 @@
 import { memo } from "react";
 import noitemStyle from "./NoItem.module.scss";
 
-export const NoItem = memo(({ content, textColor, customStyleText }) => {
-    return (
-        <p
-            className={noitemStyle["no-item-main"]}
-            style={{ color: textColor || "#6a6969", ...customStyleText }}>
-            {content || "Nothing to show here"}
-        </p>
-    );
-});
+export const NoItem = memo(
+    ({
+        content,
+        textColor,
+        customStyleText,
+        isForError = false,
+        refetchFn,
+    }) => {
+        return (
+            <>
+                <div className={noitemStyle["no-item-content"]}>
+                    <p
+                        className={noitemStyle["no-item-text"]}
+                        style={{
+                            color: textColor || "#6a6969",
+                            ...customStyleText,
+                        }}>
+                        {content || "Nothing to show here"}
+                    </p>
+                    {isForError && (
+                        <button
+                            onClick={refetchFn}
+                            className={noitemStyle["refersh-btn"]}>
+                            Retry
+                        </button>
+                    )}
+                </div>
+            </>
+        );
+    }
+);
 
 export const NoItemContiner = memo(
-    ({ content, textColor, customContainerStyle, customStyleText }) => {
+    ({ customContainerStyle, ...noItemProps }) => {
         return (
             <div
                 className={noitemStyle["no-item-container"]}
                 style={customContainerStyle}>
-                {
-                    <NoItem
-                        content={content}
-                        textColor={textColor}
-                        customStyleText={customStyleText}
-                    />
-                }
+                {<NoItem {...noItemProps} />}
             </div>
         );
     }
