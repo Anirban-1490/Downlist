e381080eeea82e397cdb4517c72f9b72852013b9
Anirban <anirbanpratihar45@gmail.com>
2023-01-24 17:57:46 +0530

    initial state change

--- Feature/Reducer/reducer.js
+++ Feature/Reducer/reducer.js
@@ -1,32 +1,32 @@
 export const reducerForSearchResult = (state, action) => {
-  if (action.type === "loading") {
-    return {
-      searchResult: [],
-      isLoading: true,
-      text: "Loading...",
-      isError: false,
-    };
-  } else if (action.type === "success") {
-    return {
-      ...state,
-      isLoading: false,
-      searchResult: action.searchResult,
-      isError: false,
-    };
-  } else if (action.type === "error") {
-    return {
-      ...state,
-      text: "No results found",
-      isLoading: false,
-      isError: true,
-    };
-  } else if (action.type === "initial") {
-    return {
-      searchResult: [],
-      isLoading: true,
-      text: "",
-      isError: false,
-    };
-  }
-  return state;
+    if (action.type === "loading") {
+        return {
+            searchResult: [],
+            isLoading: true,
+            text: "Loading...",
+            isError: false,
+        };
+    } else if (action.type === "success") {
+        return {
+            ...state,
+            isLoading: false,
+            searchResult: action.searchResult,
+            isError: false,
+        };
+    } else if (action.type === "error") {
+        return {
+            ...state,
+            text: "No results found",
+            isLoading: false,
+            isError: true,
+        };
+    } else if (action.type === "initial") {
+        return {
+            searchResult: [],
+            isLoading: undefined,
+            text: "",
+            isError: false,
+        };
+    }
+    return state;
 };
