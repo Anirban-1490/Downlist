eae5c2f7673d11f14ec13767f2035b7794e95ec1
Anirban <anirbanpratihar45@gmail.com>
2023-01-26 00:17:35 +0530

    Styled Container for childrens

---
+++ Style/EmotionComponents.jsx
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+//* styled component to produce a container for jsx elements
+export const Container = styled.div(
+    {
+        height: "auto",
+        zIndex: 1,
+        position: "relative",
+    },
+    (props) => {
+        let displyStyle;
+        displyStyle = (props.displyStyle === "flex-center-hv" && {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        }) || {
+            display: "block",
+        };
+
+        return { minHeight: props.minHeight || "100vh", ...displyStyle };
+    }
+);
