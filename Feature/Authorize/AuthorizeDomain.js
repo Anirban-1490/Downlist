df19b82a8e941c8487121e793944fb5b40fbbd1b
Anirban <anirbanpratihar45@gmail.com>
2023-02-02 16:53:27 +0530

    random ids taken from a helper function, code formatted

--- Feature/Authorize/AuthorizeDomain.js
+++ Feature/Authorize/AuthorizeDomain.js
@@ -1,15 +1,18 @@
-import { v4 as generateUUID } from "uuid";
 import { path } from "server-path";
 import axios from "axios";
+import { getRandomID } from "Feature/RandomID";
 
 export const authorizeDomain = async (token) => {
-  const uniqueID = generateUUID();
+    const uniqueID = getRandomID();
 
-  return (
-    await axios.get(`${path.domain}api/v1/auth/authorize?reqID=${uniqueID}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-  ).data;
+    return (
+        await axios.get(
+            `${path.domain}api/v1/auth/authorize?reqID=${uniqueID}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        )
+    ).data;
 };
