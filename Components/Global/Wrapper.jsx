c21274caccea0e6c45a9cad77de8215385d0d2ab
Anirban <anirbanpratihar45@gmail.com>
2023-02-10 16:24:08 +0530

    readded the .outer, .inner wrapper animation in timeline, new useEffect depnd. when route changes

--- Components/Global/Wrapper.jsx
+++ Components/Global/Wrapper.jsx
@@ -3,12 +3,14 @@
 import { useEffect, useState } from "react";
 import { gsap } from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { useRouter } from "next/router";
 gsap.registerPlugin(ScrollTrigger);
 
 export const WrapperParent = ({ children, setAnimating }) => {
     const [isDividerMount, setDividerMount] = useState(false);
-
+    const router = useRouter();
     useEffect(() => {
+        console.log(router.pathname);
         const ctx = gsap.context(() => {
             gsap.set("#__next", { overflow: "hidden" });
             // gsap.set(`.outer`, { yPercent: 100 });
@@ -22,24 +24,30 @@ export const WrapperParent = ({ children, setAnimating }) => {
                 },
             });
 
-            globalTimeline.fromTo(
-                ".divider",
-                {
-                    xPercent: 0,
-                },
-                {
-                    xPercent: -100,
-                    stagger: {
-                        each: 0.14,
-                        from: "random",
+            globalTimeline
+                .fromTo(
+                    ".divider",
+                    {
+                        xPercent: 0,
                     },
+                    {
+                        xPercent: -100,
+                        stagger: {
+                            each: 0.14,
+                            from: "random",
+                        },
+                        delay: 2,
+                    }
+                )
+                .set([".outer", ".inner", "#__next"], {
+                    overflow: "visible",
+                    height: "auto",
                     delay: 2,
-                }
-            );
+                });
         });
 
         return () => ctx.revert();
-    }, []);
+    }, [router.pathname]);
     return (
         <>
             <main className="global-wrapper">
