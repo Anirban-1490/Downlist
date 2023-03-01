4d9dbe115a95f8fdd36c72ca159a4c0b9216c368
Anirban <anirbanpratihar45@gmail.com>
2023-03-01 19:34:03 +0530

    new returned props, removed some options from useQuery

--- Feature/Authorize/Authorize.js
+++ Feature/Authorize/Authorize.js
@@ -5,31 +5,28 @@ import { authorizeDomain } from "./AuthorizeDomain";
 import { getUserToken } from "GetuserToken";
 
 export const useAuth = (isenabled, redirect = false) => {
-  console.log("did run");
-  //? custom hook to check if user is logged in or not when routing to any page
-  const router = useRouter();
-  const token = getUserToken();
+    //? custom hook to check if user is logged in or not when routing to any page
+    const router = useRouter();
+    const token = getUserToken();
 
-  const { data, isLoading } = useQuery(
-    ["user", token],
-    () => authorizeDomain(token),
-    {
-      refetchOnWindowFocus: false,
-      retry: false,
-      notifyOnChangeProps: ["data"],
-      onSettled: (data, err) => {
-        if (err) {
-          return;
+    const { data, isLoading, isFetching, isError } = useQuery(
+        ["user", token],
+        () => authorizeDomain(token),
+        {
+            refetchOnWindowFocus: false,
+            retry: 2,
+            onSettled: (data, err) => {
+                if (err) {
+                    return;
+                }
+
+                //*this will remove the current route(the signup route) from history stack so user's can't go back to it. and also redirect the user to the home page
+                if (redirect) return router.replace("/");
+            },
+            enabled: !!isenabled,
+            cacheTime: 0,
         }
+    );
 
-        //*this will remove the current route(the signup route) from history stack so user's can't go back to it. and also redirect the user to the home page
-        if (redirect) return router.replace("/");
-      },
-      enabled: !!isenabled,
-      cacheTime: 0,
-      staleTime: 100,
-    }
-  );
-
-  return [data, isLoading];
+    return [data, isLoading, isFetching, isError];
 };
