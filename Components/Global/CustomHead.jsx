291ed4ed04504a7e90f156f6f1541e6150473c38
Anirban <anirbanpratihar45@gmail.com>
2023-03-04 18:36:57 +0530

    custom head component

---
+++ Components/Global/CustomHead.jsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { serverlessPath } from "Serverlesspath";
+
+export function CustomHead({
+    description,
+    contentTitle,
+    contentType,
+    url,
+    imageUrl,
+}) {
+    return (
+        <Head>
+            <meta name="description" content={`${description}`} />
+            <title>{contentTitle}</title>
+            <meta property="og:title" content={contentTitle} />
+            <meta property="og:type" content={contentType || "website"} />
+            <meta
+                property="og:url"
+                content={`${serverlessPath.domain}${url || ""}`}
+            />
+            <meta
+                property="og:image"
+                content={imageUrl || "/DownlistLogoIcon.png"}
+            />
+            <meta property="og:description" content={description} />
+        </Head>
+    );
+}
