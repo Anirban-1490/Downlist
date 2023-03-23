db96f5c51bac204bebd9ad71cb7fd96fd03a622b
Anirban <anirbanpratihar45@gmail.com>
2023-03-23 16:43:05 +0530

    default secondary color is transparent, moved it inside a wrapper

--- Components/Global/CircularSpinner.jsx
+++ Components/Global/CircularSpinner.jsx
@@ -1,17 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 const { SpinnerCircularFixed } = require("spinners-react");
 
 export function CircularSpinner({
     enabled,
     size,
     color = "#da9109",
-    secondaryColor = "rgba(0 ,0,0,0.2)",
+    secondaryColor = "transparent",
 }) {
     return (
-        <SpinnerCircularFixed
-            enabled={enabled}
-            color={color}
-            secondaryColor={secondaryColor}
-            size={size}
-        />
+        <div css={{ display: "flex", justifyContent: "center" }}>
+            <SpinnerCircularFixed
+                enabled={enabled}
+                color={color}
+                secondaryColor={secondaryColor}
+                size={size}
+            />
+        </div>
     );
 }
