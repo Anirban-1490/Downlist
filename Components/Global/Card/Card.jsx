a174d8459e67008ea1688024a30256ee3512a5b2
Anirban <anirbanpratihar45@gmail.com>
2023-03-24 21:57:27 +0530

    <Image> component added with placeholder Blur

--- Components/Global/Card/Card.jsx
+++ Components/Global/Card/Card.jsx
@@ -1,4 +1,5 @@
 import { getUserToken } from "GetuserToken";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import cardStyle from "./Card.module.scss";
@@ -17,13 +18,25 @@ export const Card = ({
         <>
             <Link href={path ? path + `/${mal_id}` : `/`}>
                 <a className={cardStyle.cards} key={mal_id} title={mainTitle}>
+                    <Image
+                        layout="fill"
+                        src={image_url}
+                        alt={`${mainTitle}-image`}
+                        onError={(e) =>
+                            (e.currentTarget.src = "/default-placeholder.png")
+                        }
+                        placeholder="blur"
+                        blurDataURL="/default-placeholder.png"
+                    />
+
+                    {/*                     
                     <img
                         src={image_url}
                         alt={`${mainTitle}-image`}
                         onError={(e) =>
                             (e.currentTarget.src = "/default-placeholder.png")
                         }
-                    />
+                    /> */}
                     {animeType && (
                         <h3 className={cardStyle.type}>{animeType}</h3>
                     )}
