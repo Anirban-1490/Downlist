44e77ec86a0a131f0e566a87014c1e6b737ee266
Anirban <anirbanpratihar45@gmail.com>
2023-03-24 21:56:44 +0530

    Next/image configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  // Prefer loading of ES Modules over CommonJS
-  experimental: { esmExternals: "loose" },
+    // Prefer loading of ES Modules over CommonJS
+    experimental: { esmExternals: "loose" },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**.myanimelist.net",
+            },
+        ],
+    },
 };
